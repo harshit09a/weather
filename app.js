f2946d019f64fbafeca9d16cb1601c7432892e7a
harshit garg <harshit.garg09a@gmail.com>
2020-04-21 14:05:13 +0000

    fix error

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var app =express();
 var request = require("request");
+var flash = require("connect-flash");
+app.use(flash());
+app.use(require("express-session")({
+	secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+	
+}));
+app.use(function(req, res, next){
+   
+	res.locals.error = req.flash("error");
+		
+		
+   next();
+});
 app.get("/result",function(req,res){
 	var query = req.query.search;
 	request("http://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=f01b7b9da1d176a33d3ac8c2975ff12c",function(error,response,body){
@@ -8,11 +23,14 @@ app.get("/result",function(req,res){
 		if(!error&&response.statusCode==200)
 			{
 				var data = JSON.parse(body);
-				
-					
 				res.render("result.ejs",{data:data});
 			}
-		
+			else
+				{
+					req.flash("error",req.query.search+" not found try another place");
+					res.redirect("back");
+					
+				}
 	})
 })
 app.get("/",function(req,res){
